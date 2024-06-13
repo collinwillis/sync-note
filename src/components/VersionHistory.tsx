de244959f41afb61468ce32e14fbcd9d699c4186
Collin Willis <collin.willis@outlook.com>
2024-06-12 23:03:16 -0700

    Created the Version History component

---
+++ src/components/VersionHistory.tsx
@@ -0,0 +1,10 @@
+const VersionHistory = () => {
+  return (
+    <div>
+      <h2>Version History</h2>
+      {/* Implement version history UI */}
+    </div>
+  );
+};
+
+export default VersionHistory;
